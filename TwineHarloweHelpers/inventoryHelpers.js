98c01f7ef32840b5e51a7a6f89e7bd46ae1f8685
stephanotice <snsteinca@gmail.com>
2023-06-26 12:27:43 -0700

    path helper and comments

--- TwineHarloweHelpers/inventoryHelpers.js
+++ TwineHarloweHelpers/inventoryHelpers.js
@@ -1,6 +1,12 @@
-//to use, copy into your story's Javascript section
-//this assumes an existing container with id "inventory" and a Harlowe variable string array "inventory"
+/*to use, copy into your story's Javascript section
+    call in Passages with <script>getInventoryBackground();</script>
+    
+    example use in a Footer tagged passage with defined inventory span:
+        <span id="inventory"> </span>
+        < script > getInventoryBackground();</script >*/
+
 window.getInventoryText = function () {
+    //this assumes a defined Harlowe variable string array "inventory"
     const inventory = Harlowe.State.variables["inventory"].sort()
     return inventory.join("")
 }
