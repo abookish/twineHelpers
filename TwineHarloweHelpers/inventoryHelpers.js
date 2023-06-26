044c141aa3c01f1c1b173f66711dc9bd83fd540b
stephanotice <snsteinca@gmail.com>
2023-06-26 12:12:16 -0700

    inventory helpers

---
+++ TwineHarloweHelpers/inventoryHelpers.js
@@ -0,0 +1,15 @@
+window.getInventoryText = function () {
+    const inventory = Harlowe.State.variables["inventory"].sort()
+    return inventory.join("")
+}
+window.getInventoryBackground = function () {
+    const inventoryText = window.getInventoryText()
+
+    if (inventoryText.length === 0) {
+        return null
+    }
+    else {
+        $('#inventory').addClass(`${inventoryText}`)
+    }
+}
+//this assumes an existing container with id "inventory"
